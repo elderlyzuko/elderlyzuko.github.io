3dc54959cebb49a3550c3d2bc81dc2861451d1ad
Jonathan Walton <Jonathan_iMac@iMac.local>
2015-05-18 20:54:10 -0700

    sdfg

---
+++ teresaloh/main.js
@@ -0,0 +1,7 @@
+function openmenu() {
+    document.getElementById("mobile-menu").classList.remove("invisible");
+}
+
+function closemenu() {
+    document.getElementById("mobile-menu").classList.add("invisible");
+}
